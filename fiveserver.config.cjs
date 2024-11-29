401ade66aa7248c6a10ccde688299460368c609e
Nadim Ahmad <92879481+Nadim9838@users.noreply.github.com>
2024-11-29 11:44:35 +0530

    Update fiveserver.config.cjs
    To connect php exe

--- fiveserver.config.cjs
+++ fiveserver.config.cjs
@@ -2,3 +2,7 @@ module.exports = {
   highlight: true, // enable highlight feature
   injectBody: true, // enable instant update
 };
+
+module.exports = {
+  php: "C:\\xampp\\php\\php.exe"   // Windows
+}
